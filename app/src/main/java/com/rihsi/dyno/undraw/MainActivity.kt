d7d97bce6b82223e1fa9dbe15e3fd5585cf88d99
Rishav Kumar <lit2021028@iiitl.ac.in>
2022-10-05 17:06:50 +0530

    Added save feature

--- app/src/main/java/com/rihsi/dyno/undraw/MainActivity.kt
+++ app/src/main/java/com/rihsi/dyno/undraw/MainActivity.kt
@@ -4,17 +4,29 @@ import android.Manifest
 import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.Snackbar
 import com.rihsi.dyno.undraw.databinding.ActivityMainBinding
 import com.rihsi.dyno.undraw.databinding.DialogBrushSizeBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,27 +64,36 @@ class MainActivity : AppCompatActivity() {
 //                Snackbar.LENGTH_SHORT
 //            ).show()
 //        }
+        binding.ibSave.setOnClickListener {
+            requestWriteStoragePermission()
+            if(isReadStorageAllowed()){
+                lifecycleScope.launch {
+                  val flDrawingView: FrameLayout = binding.flDrawingViewContainer
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
+        }
     }
 
-//    private fun requestWriteStoragePermission() {
-//        if (ActivityCompat.shouldShowRequestPermissionRationale(
-//                this,
-//                Manifest.permission.WRITE_EXTERNAL_STORAGE
-//            )
-//        ) {
-//            Snackbar.make(
-//                binding.root,
-//                "Storage permission is required to save image",
-//                Snackbar.LENGTH_LONG
-//            ).show()
-//        } else {
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-//                CompanionObject.REQUEST_WRITE_STORAGE_PERMISSION
-//            )
-//        }
-//    }
+    private fun requestWriteStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        ) {
+            Snackbar.make(
+                binding.root,
+                "Storage permission is required to save image",
+                Snackbar.LENGTH_LONG
+            ).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                CompanionObject.REQUEST_WRITE_STORAGE_PERMISSION
+            )
+        }
+    }
 
 
 
@@ -138,6 +159,62 @@ class MainActivity : AppCompatActivity() {
             brushDialog.dismiss()
         }
         brushDialog.show()
+    }
+    private fun getBitmapFromView(view: View): Bitmap{
+        val returnedBitmap  = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }
+        else{
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+        return returnedBitmap
+    }
+    private suspend fun saveBitmapFile(mBitmap: Bitmap?): String{
+        var toSave = ""
+        withContext(Dispatchers.IO){
+            if(mBitmap !=null){
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG,90,bytes)
+
+                    val f = File(externalCacheDir?.absoluteFile.toString()
+                    + File.separator + "UndrawApp_" + System.currentTimeMillis() /1000 + ".png"
+                    )
+
+                    val fo = FileOutputStream(f)
+                    fo.write(bytes.toByteArray())
+                    fo.close()
+
+                    toSave = f.absolutePath
+
+                    runOnUiThread {
+                        if(toSave.isNotEmpty()){
+                            Toast.makeText(
+                                this@MainActivity,
+                                "File Saved Successfully : $toSave",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        else{
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Some thing went wrong in saving the file.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+                catch(e: Exception){
+                    toSave = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return toSave
     }
 }
 
